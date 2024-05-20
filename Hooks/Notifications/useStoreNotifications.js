58151a420722f0b586bbc702e68d44ea3f927e5a
Yassine_Ibhir <yibhir101@gmail.com>
2024-05-20 06:09:59 -0400

    update hooks

--- Hooks/Notifications/useStoreNotifications.js
+++ Hooks/Notifications/useStoreNotifications.js
@@ -4,11 +4,14 @@ import { useState, useEffect } from 'react';
 export default function useStoreNotifications(tok) {
 
     const notfUrl = `${process.env.EXPO_PUBLIC_NOTF_URL}/api/push_tokens/token`;
+
     console.log(notfUrl);
 
     const [tokenResponse, setTokenResponse] = useState(null);
 
     async function storePushToken(t) {
+
+        let data;
 
         try {
             console.log("fetch");
@@ -21,25 +24,26 @@ export default function useStoreNotifications(tok) {
                     token: t
                 }),
             });
-            let data = await res.json();
+            data = await res.json();
+
             if (!res.ok) {
                 throw Error(data.error);
             }
 
-            setTokenResponse(data);
+
         } catch (error) {
             console.log('error in registerForPushNotificationsAsync');
             console.log(error.message);
         }
+        finally {
+            setTokenResponse(data);
+        }
     }
-
     useEffect(() => {
         storePushToken(tok);
     }, [tok]);
-
 
     return {
         tokenResponse
     }
 }
-
